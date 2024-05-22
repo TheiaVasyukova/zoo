12454f627b603091cf9098ed09f9016e5660f06f
TheiaVasyukova <168746713+TheiaVasyukova@users.noreply.github.com>
2024-05-22 11:46:29 +0300

    add subclasses

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ class Animal:
     def eat(self):
         print(f"{self.name} кушает")
 
+class Bird(Animal):
+    def __init__(self, name, age, wings):
+        super().__init__(name, age)
+        self.wings = wings
+
+    def make_sound(self):
+        print(f"{self.name} щебечет")
+
+class Mammal(Animal):
+    def __init__(self, name, age, fur_color):
+        super().__init__(name, age)
+        self.fur_color = fur_color
+
+    def make_sound(self):
+        print(f"{self.name} рычит")
+
+class Reptile(Animal):
+    def __init__(self, name, age, type):
+        super().__init__(name, age)
+        self.type = type
+
+    def make_sound(self):
+        print(f"{self.name} шипит")
+
