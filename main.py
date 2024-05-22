e008002ee078c60594137cbdb26796cd441b4e2d
TheiaVasyukova <168746713+TheiaVasyukova@users.noreply.github.com>
2024-05-22 11:39:46 +0300

    Add basic class animal

---
+++ main.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def make_sound(self):
+        pass
+
+    def eat(self):
+        print(f"{self.name} кушает")
+
